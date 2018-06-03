4e71812626e692c21c29f6bedc83e8c7d2eab362
zephyr <zhengzefanyy@gmail.com>
2018-06-03 19:46:31 +0800

    初步完成懒加载功能

--- example/demo/src/App.vue
+++ example/demo/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img v-lazy="url">
+    <template v-for="(item, index) in imgList">
+      <img v-lazy="item.url" :key="index" :data-index="index">
+    </template>
   </div>
 </template>
 
@@ -9,8 +11,25 @@ export default {
   name: 'app',
   data () {
     return {
-      url: 'http://resource.cm233.com/img/201806/01/e4/0d4ca27cdd2a8d69dae67b684acdece4.jpg',
-      msg: 'Welcome to Your Vue.js App'
+      imgList: [
+        { url:'http://resource.cm233.com/chapter/b_197/cf208a15bbc12e5bdf6b314e666a0d58.jpg?size=900x2000' },
+        { url:'http://resource.cm233.com/chapter/b_197/8f48058196f38959c85ae891a402329c.jpg?size=900x700' },
+        { url:'http://resource.cm233.com/chapter/b_197/8f48058196f38959c85ae891a402329c.jpg?size=900x700' },
+        { url:'http://resource.cm233.com/chapter/b_197/8f48058196f38959c85ae891a402329c.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/20a2932858c81953b984e54fc4cfab0c.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/a202d20f7032bbe19b362bc011174402.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/aea07b905b54c0c562281c3c0dadcaf8.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/33f57cb6fbc4fe1f7520975c20a08e7c.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/efc53d281ca66b7a5b5135b018731630.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/e03cbca147f83f2eb8ac6126bf397f71.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/d2b12b1d30a4d632dd23d2c888eb733a.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/1df2c9e6fc0e25721fe5c79bb43fa0c8.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/57fd81bc03e58ec6b3faef65e69db14d.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/9d728dc2152260678a81590c274eb274.jpg?size=900x700' },
+        { url: 'http://resource.cm233.com/chapter/b_197/09bfa66538f4746fbe62ff62a0e7d6c8.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/375e157937e9e5f25efcf9be037aa1af.jpg?size=900x2000' },
+        { url: 'http://resource.cm233.com/chapter/b_197/00854dca6290cfbdc3be40517649562a.jpg?size=900x700' },
+      ],
     }
   }
 }
@@ -22,25 +41,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50;  
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+img {
+  background: red;
+  width: 100%;
+  min-height: 600px;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 export default {
-    install(Vue, options={}) {
+    install(Vue, options = {}) {
         const IMG = 'data:img/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEXs7Oxc9QatAAAACklEQVQI12NgAAAAAgAB4iG8MwAAAABJRU5ErkJggg=='
+        const EVENTS = ['wheel', 'scroll', 'touchmove']
 
-        function addListener(el, binding, vnode, oldVnode) {            
-            el.src = IMG            
-            setTimeout(()=>{
-                console.log(binding.value)
-                el.src = binding.value
-            }, 1000)
+        function throttle(action, delay) {
+            let timeout = null
+            let lastRun = 0
+            return function () {
+                if (timeout) {
+                    return
+                }
+                let elapsed = Date.now() - lastRun
+                let context = this
+                let args = arguments
+                let runCallback = function () {
+                    lastRun = Date.now()
+                    timeout = false
+                    action.apply(context, args)
+                }
+                if (elapsed >= delay) {
+                    runCallback()
+                } else {
+                    timeout = setTimeout(runCallback, delay)
+                }
+            }
+        }
+
+        function addListener(el, binding, vnode, oldVnode) {
+            // el.src = IMG
+            Vue.nextTick(() => {
+                EVENTS.forEach((event) => {
+                    throttle(
+                        el.addEventListener(event, (evt) => {
+                            const rect = evt.target.getBoundingClientRect()
+                            if (
+                                (rect.top < window.innerHeight && rect.bottom > 0) &&
+                                (rect.left < window.innerWidth && rect.right > 0)
+                            ) {
+                                console.log(el)
+                                console.log('inView')
+                                el.src = binding.value
+                            }
+                        }, false)
+                    , 300)
+                })
+            })
         }
 
         Vue.directive('lazy', {
