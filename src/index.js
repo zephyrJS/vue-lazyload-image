ae8c8837925cff4b5c113fb7b6a80d2c0b8a955e
zephyr <zhengzefanyy@gmail.com>
2018-06-03 21:28:40 +0800

    减少代码耦合度，添加缓存列表，如果图片已加载，则不再加载

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+Array.prototype.remove = function(item){
+    if(!this.length) return
+    const index = this.indexOf(item)
+    if(index !== -1) {
+        return this.splice(index, 1)
+    }
+}
 export default {
     install(Vue, options = {}) {
         const IMG = 'data:img/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEXs7Oxc9QatAAAACklEQVQI12NgAAAAAgAB4iG8MwAAAABJRU5ErkJggg=='
         const EVENTS = ['wheel', 'scroll', 'touchmove']
+
+        const opt = {
+            hasBind: false
+        }
+
+        const Listeners = []
 
         function throttle(action, delay) {
             let timeout = null
@@ -26,24 +39,77 @@ export default {
             }
         }
 
-        function addListener(el, binding, vnode, oldVnode) {
-            // el.src = IMG
-            Vue.nextTick(() => {
-                EVENTS.forEach((event) => {
-                    throttle(
-                        el.addEventListener(event, (evt) => {
-                            const rect = evt.target.getBoundingClientRect()
-                            if (
-                                (rect.top < window.innerHeight && rect.bottom > 0) &&
-                                (rect.left < window.innerWidth && rect.right > 0)
-                            ) {
-                                console.log(el)
-                                console.log('inView')
-                                el.src = binding.value
-                            }
-                        }, false)
-                    , 300)
+        const Tools = {
+            on: (el, evt, callback) => {
+                el.addEventListener(evt, callback, false)
+            },
+            off: (el, evt, callback) => {
+                el.removeEventListener(evt, callback, false)
+            }
+        }
+
+        function events(el, bindType) {
+            if(bindType) {
+                EVENTS.forEach((evt)=>{
+                    Tools.on(el, evt, loadImg)
                 })
+            }else {
+                EVENTS.forEach((evt)=>{
+                    Tools.off(el, evt, loadImg)
+                })
+            }
+        }
+
+        function loadImg() {
+            Listeners.forEach(listener => {
+                checkImg(listener)
+            })
+        }
+
+        function checkImg(listener) {
+            if(isInView(listener)) {
+                console.log('load')
+                listener.el.src = listener.src
+                Listeners.remove(listener)
+                console.log(Listeners.length)
+            }
+        }
+
+        function isInView(listener) {
+            const rect = listener.el.getBoundingClientRect()
+            return (rect.top < window.innerHeight && rect.bottom > 0) &&
+            (rect.left < window.innerWidth && rect.right > 0)
+        }
+
+        function addListener(el, binding, vnode, oldVnode) {
+            let hasBind = opt.hasBind
+
+            Vue.nextTick(() => {
+                Listeners.push({
+                    src: binding.value,
+                    el: el,
+                })
+                loadImg()
+                if(Listeners.length > 0 && !hasBind) {
+                    console.log('first listen scroll')
+                    hasBind = true
+                    events(window, true)
+                }
+                // EVENTS.forEach((event) => {
+                //     throttle(
+                //         el.addEventListener(event, (evt) => {
+                //             const rect = evt.target.getBoundingClientRect()
+                //             if (
+                //                 (rect.top < window.innerHeight && rect.bottom > 0) &&
+                //                 (rect.left < window.innerWidth && rect.right > 0)
+                //             ) {
+                //                 console.log(el)
+                //                 console.log('inView')
+                //                 el.src = binding.value
+                //             }
+                //         }, false)
+                //     , 300)
+                // })
             })
         }
 
